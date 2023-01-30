d8d64d3e6e600c9f561d4931684fa8f0a132cce6
Jacob Nave <jnave@mercury.com>
2023-01-30 10:41:09 -0800

    ch10 exercises

--- ch10.hs
+++ ch10.hs
@@ -13,6 +13,7 @@ theDatabase =
              (fromGregorian 1911 5 1)
         (secondsToDiffTime 34123))
     , DbNumber 9001
+    , DbNumber 12
     , DbString "Hello, world!"
     , DbDate (UTCTime
              (fromGregorian 1921 5 1)
@@ -35,3 +36,32 @@ foldDbHelper arr _ = arr
 
 foldDbDate :: [DatabaseItem] -> [UTCTime]
 foldDbDate items = foldl foldDbHelper [] items
+
+foldDbNumber :: [DatabaseItem] -> [Integer]
+foldDbNumber items = foldr ((++) . foldHelper) [] items 
+    where 
+        foldHelper :: DatabaseItem -> [Integer]
+        foldHelper item = case item of
+            DbNumber n -> [n]
+            _ -> []
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent items = foldr (\x y -> if x > y then x else y) (head filteredItems) (tail filteredItems) 
+    where 
+        filteredItems = ((foldDbDate) items)
+
+sumDb :: [DatabaseItem] -> Integer 
+sumDb items = foldr (\x y -> x + y) 0 (foldDbNumber items)
+
+avgDb :: [DatabaseItem] -> Double
+avgDb items = fromIntegral (sumDb items) / fromIntegral (length $ foldDbNumber items)
+
+myReverse :: [a] -> [a]
+myReverse items = foldl (\x y -> flip (:) x y) [] items
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter func items = foldl (\x y -> x ++ filterVal y) [] items
+    where 
+        filterVal val = case func val of
+            True -> [val]
+            False -> []

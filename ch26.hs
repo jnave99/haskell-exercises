2be5b2b98245306a0fa9e62616f9715f4ea5a90f
Jacob Nave <jnave@mercury.com>
2023-02-17 11:36:32 -0800

    statet

--- ch26.hs
+++ ch26.hs
@@ -1,3 +1,4 @@
+import Control.Monad 
 
 newtype EitherT e m a =
     EitherT { runEitherT :: m (Either e a) }
@@ -25,3 +26,25 @@ swapEither (Right a) = Left a
 
 swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e 
 swapEitherT (EitherT ema) = EitherT $ (fmap swapEither ema)
+
+newtype StateT s m a =
+    StateT { runStateT :: s -> m (a,s) }
+
+instance (Functor m) => Functor (StateT s m) where
+    fmap f m = (fmap f m)
+ 
+instance (Monad m) => Applicative (StateT s m) where 
+    pure a = StateT (\s -> (pure (a, s)))
+    (StateT smf) <*> (StateT sma) = StateT stateFunc
+        where 
+            stateFunc s = do
+                fstResult <- (smf s)
+                sndResult <- (sma (snd fstResult))
+                return ((fst fstResult) (fst sndResult), snd sndResult)
+                    
+instance (Monad m) => Monad (StateT s m) where 
+    return = pure 
+    (StateT sma) >>= f = 
+        StateT $ \s -> do 
+            a <- sma s 
+            runStateT (f (fst a)) s
